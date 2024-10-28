ee058117984c9035943d146afb4d456ec4a94088
João Souza <joao.souza@fitbank.com.br>
2024-10-28 10:55:46 -0300

    feat: implementação inicial de teste

--- Program.cs
+++ Program.cs
@@ -1,9 +1,17 @@
+using ApiTesteUds.Application.Interfaces.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using MinimalApiTesteUds.Application.Interfaces.Services;
+using MinimalApiTesteUds.Application.Services;
+using MinimalApiTesteUds.Infraestructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddHttpClient<IExternalApiService, ExternalApiService>();
+builder.Services.AddScoped<IExternalApiService, ExternalApiService>();
+builder.Services.AddScoped<IExternalApiRepository, ExternalApiRepository>();
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -16,10 +24,19 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+
+app.MapGet("/api/people", async (string? name, string? gender, [FromServices]IExternalApiService service) =>
+{
+    var result = await service.GetPersonOrFiltersAsync(name, gender);
+    return Results.Ok(result);
+});
 
 app.Run();
